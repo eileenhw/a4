f5bed93eb7181e8bed5b01125b1e28685cbc5a79
eileenhw <hwangem@uci.edu>
2025-06-02 13:11:29 -0700

    file added

---
+++ a4_part1/a4_part1/a4_p1.py
@@ -0,0 +1,29 @@
+# NAME
+# EMAIL
+# STUDENT ID
+
+import urllib
+import bookmark_connection as bmc
+from bookmark_connection import BookmarkProtocol
+
+"""
+The following code snippets can be used to help you prepare your test function:
+The url to use for testing.
+Be sure to run bookmark_server.py before making requests!
+
+url = 'http://localhost:8000'
+
+The format to use for your request data.
+Don't forget to encode before sending a request!
+
+json = {'data':bmc.BookmarkProtocol.format(
+                                BookmarkProtocol(BookmarkProtocol.ADD, data))}
+"""
+
+def http_api_test():
+    # TODO: write your http connection code here. You can use the above snippets to help
+    pass
+
+if __name__ == '__main__':
+    # TODO: call your test code from here. You might try writing a few different url tests.
+    pass
